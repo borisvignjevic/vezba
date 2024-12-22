93e7f3857a7c89c1f732860ea2cfeb5896ceae85
borisvignjevic <bokivignjevic@gmail.com>
2024-12-22 22:50:32 +0100

    * Finished main python and data.json

--- Python/12/main.py
+++ Python/12/main.py
@@ -1,1 +1,16 @@
 
+import json
+
+with open("data.json", 'r') as file:
+    data = json.load(file)
+    data.append({
+        "name": "Vuk Vignjevic",
+        "age": 12,
+        "height": 150,
+        "gander": "male"
+    })
+
+print(data)
+
+with open("data.json", 'w')as file:
+    json.dump(data, file, indent=4)
